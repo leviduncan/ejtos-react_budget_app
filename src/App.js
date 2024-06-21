5409d6b9f9bf2b63991e1d16e1b8b7c71be02704
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-06-21 14:17:11 -0400

    Initial updates

--- src/App.js
+++ src/App.js
@@ -9,37 +9,50 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                <div className='row mt-3'>
+                    {
+                        <div className="col-sm">
+                            <Budget />
+                        </div>
+                    }
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    {
+                        <div className="col-sm">
+                            <Remaining />
+                        </div>
+                    }
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                    {
+                        <div className="col-sm">
+                            <ExpenseTotal />
+                        </div>
+                    }
                 </div>
+                <div className="row">
+                    <h2>Allocation</h2>
+                    {
+                        <ExpenseList />
+                    }
+                </div>
+
+                <div className="row">
+                    <h2>Change allocation</h2>
+                    {
+                        <AllocationForm />
+                    }
+                </div>
+
+
             </div>
         </AppProvider>
     );
